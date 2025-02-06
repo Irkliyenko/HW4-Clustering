640d597dfbec922330620915b54e8cc90f1b6272
Iryna Irkliyenko <iryna.irklyenko@ucsf.edu>
2025-02-06 13:52:13 -0800

    test

--- test/test_kmeans.py
+++ test/test_kmeans.py
@@ -1,1 +1,30 @@
-# Write your k-means unit tests here
+import pytest 
+import numpy as np
+
+from cluster import (KMeans, make_clusters)
+
+def test_recognition_of_number_cluster():
+    """Test if KMeans correctly recognizes the number of clusters."""
+    n = 5
+    clusters, _ = make_clusters(k=n, scale=1)
+
+    k = KMeans(k=n)
+    k.fit(clusters)
+    centroids = k.get_centroids()
+
+    assert len(centroids) == n, "KMeans don't recognize number of centroids"
+
+
+def test_zero_clusters():
+    """Test if KMeans raises an error when k=0."""
+    with pytest.raises(ValueError, match=r"Number of clusters \(k\) must be greater than 0."):
+        k = KMeans(k=0)
+
+
+
+
+
+
+
+
+
--- test/test_silhouette.py
+++ test/test_silhouette.py
@@ -1,1 +1,16 @@
-# write your silhouette score unit tests here
+import pytest
+import numpy as np
+from cluster import (KMeans, 
+                     Silhouette,
+                     make_clusters)
+
+def test_silhouette_score_test():
+
+    clusters, _ = make_clusters(k=3, scale=1)
+    k = KMeans(k=3)
+    s = Silhouette()
+    k.fit(clusters)
+    pred = k.predict(clusters)
+    score = s.compare_with_sklearn(clusters, pred)
+
+    assert np.isclose(score[0], score[1], atol=1e-2), "Custom silhouette score is different from Sklearn"
